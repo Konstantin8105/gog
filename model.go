d1fe4cf5424250d75cfeaccfc59fc00fc746501f
Konstantin8105 <konstantin8105@gmail.com>
2021-11-25 13:27:44 +0300

    model: add TODOs

--- model.go
+++ model.go
@@ -1,13 +1,18 @@
 package gog
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
+// Model of points, lines, arcs for prepare of triangulation
 type Model struct {
 	Points []Point  // Points is slice of points
-	Lines  [][2]int // Lines store 2 index of Points
-	Arcs   [][3]int // Arcs store 3 index of Points
+	Lines  [][3]int // Lines store 2 index of Points and last for tag
+	Arcs   [][4]int // Arcs store 3 index of Points and last for tag
 }
 
+// AddPoint return index in model slice point
 func (m *Model) AddPoint(p Point) (index int) {
 	// search in exist points
 	for i := range m.Points {
@@ -21,17 +26,19 @@ func (m *Model) AddPoint(p Point) (index int) {
 	return len(m.Points) - 1
 }
 
-func (m *Model) AddLine(start, end Point) {
+// AddLine add line into model with specific tag
+func (m *Model) AddLine(start, end Point, tag int) {
 	// add points
 	var (
 		st = m.AddPoint(start)
 		en = m.AddPoint(end)
 	)
 	// add line
-	m.Lines = append(m.Lines, [2]int{st, en})
+	m.Lines = append(m.Lines, [3]int{st, en, tag})
 }
 
-func (m *Model) AddArc(start, middle, end Point) {
+// AddArc add arc into model with specific tag
+func (m *Model) AddArc(start, middle, end Point, tag int) {
 	// add points
 	var (
 		st = m.AddPoint(start)
@@ -39,10 +46,11 @@ func (m *Model) AddArc(start, middle, end Point) {
 		en = m.AddPoint(end)
 	)
 	// add arc
-	m.Arcs = append(m.Arcs, [3]int{st, mi, en})
+	m.Arcs = append(m.Arcs, [4]int{st, mi, en, tag})
 }
 
-func (m *Model) AddCircle(xc, yc, r float64) {
+// AddCircle add arcs based on circle geometry into model with specific tag
+func (m *Model) AddCircle(xc, yc, r float64, tag int) {
 	// add points
 	var (
 		up    = Point{X: xc, Y: yc + r}
@@ -51,9 +59,113 @@ func (m *Model) AddCircle(xc, yc, r float64) {
 		right = Point{X: xc + r, Y: yc}
 	)
 	// add arcs
-	m.AddArc(down, left, up)
-	m.AddArc(up, right, down)
+	m.AddArc(down, left, up, tag)
+	m.AddArc(up, right, down, tag)
+}
+
+// Intersection change model with finding all model intersections
+func (m *Model) Intersection() {
+	for _, f := range []func() int{
+		// point-line intersection
+		// TODO
+
+		// point-arc intersection
+		// TODO
+
+		// line-line intersection
+		func() (ai int) {
+			intersect := make([]bool, len(m.Lines))
+			size := len(m.Lines)
+			for il := 0; il < size; il++ {
+				for jl := 0; jl < size; jl++ {
+					// ignore intersection lines
+					if il <= jl || intersect[il] || intersect[jl] {
+						continue
+					}
+					pi, st = SegmentAnalisys(
+						m.Lines[il][0], m.Lines[il][1],
+						m.Lines[jl][0], m.Lines[jl][1],
+					)
+					// not acceptable zero length lines
+					if st.Has(ZeroLengthSegmentA) ||
+						st.Has(ZeroLengthSegmentB) {
+						panic(fmt.Errorf("zero lenght error: %v", st))
+					}
+					// intersection on line A
+					//
+					// for cases - no need update the line:
+					// OnPoint0SegmentA, OnPoint1SegmentA
+					//
+					if st.Has(OnSegmentA) {
+						intersection[il] = true
+						tag := m.Lines[il][2]
+						m.AddLine(m.Lines[il][0], pi[0], tag)
+						m.AddLine(pi[0], m.Lines[il][1], tag)
+					}
+					// intersection on line B
+					//
+					// for cases - no need update the line:
+					// OnPoint0SegmentB, OnPoint1SegmentB
+					//
+					if st.Has(OnSegmentB) {
+						intersection[jl] = true
+						tag := m.Lines[jl][2]
+						m.AddLine(m.Lines[jl][0], pi[0], tag)
+						m.AddLine(pi[0], m.Lines[jl][1], tag)
+					}
+				}
+			}
+			for i := size - 1; 0 <= i; i++ {
+				if intersection[i] {
+					// add to amount intersections
+					ai++
+					// remove intersection line
+					m.Lines = append(m.Lines[:i], m.Lines[i+1:]...)
+				}
+			}
+			return
+		},
+
+		// arc-line intersection
+		// TODO
+
+	} {
+		if 0 < f() {
+			// repeat if intersections is not found
+			m.Intersection()
+			return
+		}
+	}
+}
+
+func (m *Model) RemovePoint() {
+	// TODO
+}
+
+func (m *Model) RemoveEmptyPoints() {
+	// TODO
 }
 
 func (m *Model) Split() {
+	// TODO
+}
+
+// MinPointDistance return minimal between 2 points
+func (m Model) MinPointDistance() (distance float64) {
+	distance = math.MaxFloat64 // default value of distance
+	for i := range m.Points {
+		for j := range m.Points {
+			// ignore
+			if i <= j {
+				continue
+			}
+			// calculation
+			distance = math.Max(distance,
+				math.Hypot(
+					m.Points[i].X-m.Points[j].X,
+					m.Points[i].Y-m.Points[j].Y,
+				))
+		}
+	}
+	return
 }
