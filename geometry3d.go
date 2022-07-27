d72b0c5c66038d8b29f14a25fa3e5d994e89894e
Konstantin8105 <konstantin8105@gmail.com>
2022-07-27 14:43:34 +0300

    add geometry3d

---
+++ geometry3d.go
@@ -0,0 +1,329 @@
+package gog
+
+import (
+	"math"
+
+	"github.com/Konstantin8105/pow"
+)
+
+const Eps3D = 1e-5
+
+// Space 3D
+//
+//	+-----------+-------+-------+-----------+
+//	|           | Point | Line2 | Triangle3 |
+//	+-----------+-------+-------+-----------+
+//	| Point     |   V   |   V   |           |
+//	+-----------+-------+-------+-----------+
+//	| Line2     |   -   |       |           |
+//	+-----------+-------+-------+-----------+
+//	| Triangle3 |   -   |   -   |           |
+//	+-----------+-------+-------+-----------+
+
+type Point3d [3]float64
+
+func Distance3d(p0, p1 Point3d) float64 {
+	return math.Sqrt(pow.E2(p0[0]-p1[0]) +
+		pow.E2(p0[1]-p1[1]) +
+		pow.E2(p0[2]-p1[2]))
+	// https://arxiv.org/pdf/1904.09481.pdf
+}
+
+func PointPoint3d(
+	p0 Point3d,
+	p1 Point3d,
+) (
+	intersect bool,
+) {
+	for i := range p0 {
+		if Eps < math.Abs(p0[i]-p1[i]) {
+			return false
+		}
+	}
+	return Distance3d(p0, p1) < Eps3D
+}
+
+func PointLine3d(
+	p Point3d,
+	l0, l1 Point3d,
+) (
+	intersect bool,
+) {
+	// is point on point line
+	for _, v := range [2]*Point3d{&l0, &l1} {
+		if PointPoint3d(p, *v) {
+			return
+		}
+	}
+	// line zero lenght
+	if ZeroLine3d(l0, l1) {
+		return
+	}
+	// is point in line box
+	for i := range p {
+		if p[i] < l0[i] && p[i] < l1[i] {
+			return
+		}
+		if l0[i] < p[i] && l1[i] < p[i] {
+			return
+		}
+	}
+	// is point on line
+	return true
+}
+
+func ZeroLine3d(
+	l0, l1 Point3d,
+) (
+	zero bool,
+) {
+	return Distance3d(l0, l1) < Eps3D
+}
+
+func PointLineRatio3d(
+	l0, l1 Point3d,
+	ratio float64,
+) (
+	p Point3d,
+) {
+	for i := 0; i < 3; i++ {
+		// p[i] = l0[i] + ratio*(l1[i]-l0[i])
+		p[i] = math.FMA(ratio, l1[i]-l0[i], l0[i])
+	}
+	return
+}
+
+const oneThree = 1.0 / 3.0
+
+// Point on line corner ignored
+func LineLine3d(
+	a0, a1 Point3d,
+	b0, b1 Point3d,
+) (
+	ratioA, ratioB float64,
+	intersect bool,
+) {
+	// Lina a:
+	//	x = xa0 + Ka * (xa1-xa0)
+	//	y = ya0 + Ka * (ya1-ya0)
+	//	z = za0 + Ka * (za1-za0)
+	//
+	// Lina b:
+	//	x = xb0 + Kb * (xb1-xb0)
+	//	y = yb0 + Kb * (yb1-yb0)
+	//	z = zb0 + Kb * (zb1-zb0)
+	//
+	// Intersection point:
+	//	xa0 + Ka * (xa1-xa0) = xb0 + Kb * (xb1-xb0)
+	//
+	// System of equations:
+	//	Ka * (a1x-a0x) - Kb * (b1x-b0x) = b0x - a0x
+	//	Ka * (a1y-a0y) - Kb * (b1y-b0y) = b0y - a0y
+	//	Ka * (a1z-a0z) - Kb * (b1z-b0z) = b0z - a0z
+	sys := [3][3]float64{
+		{a1[0] - a0[0], -(b1[0] - b0[0]), b0[0] - a0[0]},
+		{a1[1] - a0[1], -(b1[1] - b0[1]), b0[1] - a0[1]},
+		{a1[2] - a0[2], -(b1[2] - b0[2]), b0[2] - a0[2]},
+	}
+	var Ka, Kb []float64
+	for _, v := range [3][2]int{{0, 1}, {1, 2}, {2, 0}} {
+		x, y, err := Linear(
+			sys[v[0]][0], sys[v[0]][1], sys[v[0]][2],
+			sys[v[1]][0], sys[v[1]][1], sys[v[1]][2],
+		)
+		if err != nil {
+			continue
+		}
+		Ka = append(Ka, x)
+		Kb = append(Kb, y)
+	}
+	if len(Ka) < 1 {
+		return
+	}
+	for i := range Ka {
+		ratioA = math.FMA(Ka[i], 1.0/float64(len(Ka)), ratioA)
+		ratioB = math.FMA(Kb[i], 1.0/float64(len(Kb)), ratioB)
+	}
+	intersect = true
+	return
+}
+
+// Plane equation `A*x+B*y+C*z+D=0`
+func Plane(
+	p1, p2, p3 Point3d,
+) (
+	A, B, C, D float64,
+) {
+	var (
+		a1 = p2[0] - p1[0]
+		b1 = p2[1] - p1[1]
+		c1 = p2[2] - p1[2]
+		a2 = p3[0] - p1[0]
+		b2 = p3[1] - p1[1]
+		c2 = p3[2] - p1[2]
+	)
+	// algorithm float
+	// A = b1*c2 - b2*c1
+	// B = a2*c1 - a1*c2
+	// C = a1*b2 - b1*a2
+	// D = (-A*x1 - B*y1 - C*z1)
+
+	// algorithm FMA
+	A = math.FMA(b1, c2, -b2*c1)
+	B = math.FMA(a2, c1, -a1*c2)
+	C = math.FMA(a1, b2, -b1*a2)
+	D = math.FMA(-A, p1[0], math.FMA(-B, p1[1], -C*p1[2]))
+	return
+}
+
+func PointOnPlane3d(
+	A, B, C, D float64,
+	p Point3d,
+) (
+	on bool,
+) {
+	return math.Abs(math.FMA(A, p[0], math.FMA(B, p[1], math.FMA(C, p[2], D)))) < Eps3D
+}
+
+func ZeroTriangle3d(
+	t0, t1, t2 Point3d,
+) (
+	zero bool,
+) {
+	return ZeroLine3d(t0, t1) || ZeroLine3d(t1, t2) || ZeroLine3d(t2, t0)
+}
+
+// do not check point on triangle edge
+func PointTriangle3d(
+	p Point3d,
+	t0, t1, t2 Point3d,
+) (
+	intersect bool,
+) {
+	A, B, C, D := Plane(t0, t1, t2)
+	if !PointOnPlane3d(A, B, C, D, p) {
+		// point is not plane
+		return
+	}
+	// point on plane
+	for _, v := range [3][4]*Point3d{
+		{&t0, &p, &t1, &t2},
+		{&t1, &p, &t2, &t0},
+		{&t2, &p, &t0, &t1},
+	} {
+		var rA, rB float64
+		rA, rB, intersect = LineLine3d(*v[0], *v[1], *v[2], *v[3])
+		if !intersect || rA < 0.0 || rB < 0.0 {
+			// point is not in triangle
+			return false
+		}
+	}
+	return true
+}
+
+// line intersect triangle in one point
+func LineTriangle3dI1(
+	l0, l1 Point3d,
+	t0, t1, t2 Point3d,
+) (
+	intersect bool,
+	pi []Point3d,
+) {
+	A, B, C, D := Plane(t0, t1, t2)
+	if PointOnPlane3d(A, B, C, D, l0) && PointOnPlane3d(A, B, C, D, l1) {
+		// Lines points on Plane
+		return
+	}
+
+	// Line intersect Triangle on one point
+	// div := ((l1[0]-l0[0])*A + (l1[1]-l0[1])*B + (l1[2]-l0[2])*C)
+	div := math.FMA(l1[0]-l0[0], A, math.FMA(l1[1]-l0[1], B, (l1[2]-l0[2])*C))
+	if math.Abs(div) < Eps3D {
+		return
+	}
+	// Ka := (A*l0[0] + B*l0[1] + C*l0[2] + D) / (-div)
+	Ka := math.FMA(A, l0[0], math.FMA(B, l0[1], math.FMA(C, l0[2], D))) / (-div)
+
+	if Ka < 0 || 1 < Ka {
+		return
+	}
+
+	p := PointLineRatio3d(l0, l1, Ka)
+
+	if !PointTriangle3d(p, t0, t1, t2) {
+		return
+	}
+	intersect = true
+	pi = append(pi, p)
+	return
+}
+
+// Line on triangle plane
+// Line is not zero
+// ignore triangle point on line
+func LineTriangle3dI2(
+	l0, l1 Point3d,
+	t0, t1, t2 Point3d,
+) (
+	intersect bool,
+	pi []Point3d,
+) {
+	A, B, C, D := Plane(t0, t1, t2)
+	if !(PointOnPlane3d(A, B, C, D, l0) && PointOnPlane3d(A, B, C, D, l1)) {
+		// line not on triangle plane
+		return
+	}
+	// intersection line inside triangle
+	for _, v := range [2]*Point3d{&l0, &l1} {
+		if PointTriangle3d(*v, t0, t1, t2) {
+			intersect = true
+			pi = append(pi, *v)
+		}
+	}
+	// line outside triangle
+	for _, v := range [3][2]*Point3d{{&t0, &t1}, {&t1, &t2}, {&t2, &t0}} {
+		if rA, rB, ill := LineLine3d(l0, l1, *v[0], *v[1]); ill &&
+			0 <= rA && rA <= 1 && 0 <= rB && rB <= 1 {
+			intersect = true
+			p := PointLineRatio3d(l0, l1, rA)
+			pi = append(pi, p)
+		}
+	}
+	return
+}
+
+// do not intersect with egdes
+func TriangleTriangle3d(
+	a0, a1, a2 Point3d,
+	b0, b1, b2 Point3d,
+) (
+	intersect bool,
+	pi []Point3d,
+) {
+	var pis []Point3d
+	for i := 0; i < 2; i++ {
+		if i == 1 {
+			a0, a1, a2, b0, b1, b2 = b0, b1, b2, a0, a1, a2 // swap
+		}
+		for _, f := range [2]func(Point3d, Point3d, Point3d, Point3d, Point3d) (bool, []Point3d){
+			LineTriangle3dI1,
+			LineTriangle3dI2,
+		} {
+			for _, v := range [3][2]*Point3d{{&a0, &a1}, {&a1, &a2}, {&a2, &a0}} {
+				ilt, pit := f(*v[0], *v[1], b0, b1, b2)
+				if ilt {
+					intersect = intersect || ilt
+					pis = append(pis, pit...)
+				}
+			}
+		}
+	}
+	for i := range pis {
+		if PointTriangle3d(pis[i], a0, a1, a2) &&
+			PointTriangle3d(pis[i], b0, b1, b2) {
+			pi = append(pi, pis[i])
+			continue
+		}
+	}
+	return
+}
---
+++ geometry3d_test.go
@@ -0,0 +1,320 @@
+package gog
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func ExamplePlane() {
+	delta := 1.0
+	for i := 0; i < 30; i++ {
+		A, B, C, D := Plane(
+			Point3d{-10, -1, 10}, Point3d{10, -1, 10}, Point3d{0, 1 + delta, 0},
+		)
+		fmt.Fprintf(os.Stdout, "%.1e\t%e %e %e %e\n", delta, A, B, C, D)
+		delta /= 10.0
+	}
+
+	// Output:
+	// 1.0e+00	-0.000000e+00 2.000000e+02 6.000000e+01 -4.000000e+02
+	// 1.0e-01	-0.000000e+00 2.000000e+02 4.200000e+01 -2.200000e+02
+	// 1.0e-02	-0.000000e+00 2.000000e+02 4.020000e+01 -2.020000e+02
+	// 1.0e-03	-0.000000e+00 2.000000e+02 4.002000e+01 -2.002000e+02
+	// 1.0e-04	-0.000000e+00 2.000000e+02 4.000200e+01 -2.000200e+02
+	// 1.0e-05	-0.000000e+00 2.000000e+02 4.000020e+01 -2.000020e+02
+	// 1.0e-06	-0.000000e+00 2.000000e+02 4.000002e+01 -2.000002e+02
+	// 1.0e-07	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-08	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-09	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-10	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-11	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-12	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-13	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-14	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-15	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-16	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-17	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-18	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-19	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-20	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-21	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-22	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-23	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-24	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-25	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-26	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-27	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-28	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+	// 1.0e-29	-0.000000e+00 2.000000e+02 4.000000e+01 -2.000000e+02
+}
+
+func ExamplePointPoint3d() {
+	p := Point3d{1, 1, 1}
+	delta := 1.0
+	for i := 0; i < 30; i++ {
+		pf := Point3d{1, 1, 1 + delta}
+		value := PointPoint3d(p, pf)
+		fmt.Fprintf(os.Stdout, "%.1e\t%v\n", delta, value)
+		delta /= 10.0
+	}
+
+	// Output:
+	// 1.0e+00	false
+	// 1.0e-01	false
+	// 1.0e-02	false
+	// 1.0e-03	false
+	// 1.0e-04	false
+	// 1.0e-05	false
+	// 1.0e-06	false
+	// 1.0e-07	false
+	// 1.0e-08	false
+	// 1.0e-09	false
+	// 1.0e-10	false
+	// 1.0e-11	true
+	// 1.0e-12	true
+	// 1.0e-13	true
+	// 1.0e-14	true
+	// 1.0e-15	true
+	// 1.0e-16	true
+	// 1.0e-17	true
+	// 1.0e-18	true
+	// 1.0e-19	true
+	// 1.0e-20	true
+	// 1.0e-21	true
+	// 1.0e-22	true
+	// 1.0e-23	true
+	// 1.0e-24	true
+	// 1.0e-25	true
+	// 1.0e-26	true
+	// 1.0e-27	true
+	// 1.0e-28	true
+	// 1.0e-29	true
+}
+
+func Test3D(t *testing.T) {
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			switch len(tc.ps) {
+			case 3:
+				var (
+					p0 = Point3d{tc.ps[0].X, tc.ps[0].Y, 0}
+					p1 = Point3d{tc.ps[1].X, tc.ps[1].Y, 0}
+					p2 = Point3d{tc.ps[2].X, tc.ps[2].Y, 0}
+				)
+				intersect := PointLine3d(
+					p0,
+					p1, p2,
+				)
+				tint := tc.itA.Has(OnSegment) || tc.itB.Has(OnSegment)
+				if intersect != tint {
+					t.Errorf("not same")
+				}
+			case 4:
+				var (
+					p0 = Point3d{tc.ps[0].X, tc.ps[0].Y, 0}
+					p1 = Point3d{tc.ps[1].X, tc.ps[1].Y, 0}
+					p2 = Point3d{tc.ps[2].X, tc.ps[2].Y, 0}
+					p3 = Point3d{tc.ps[3].X, tc.ps[3].Y, 0}
+				)
+				rA, rB, intersect := LineLine3d(
+					p0, p1,
+					p2, p3,
+				)
+				if intersect &&
+					(rA < 0 || 1 < rA || rB < 0 || 1 < rB) {
+					intersect = false
+				}
+				tint := tc.itA.Has(OnSegment) || tc.itB.Has(OnSegment)
+				if intersect != tint {
+					t.Errorf("not same: %f %f. %v", rA, rB, tc.pi)
+				}
+			}
+		})
+	}
+	for _, tc := range triaTests {
+		t.Run(tc.name, func(t *testing.T) {
+			p := Point3d{tc.pt.X, tc.pt.Y, 0}
+			tri0 := Point3d{tc.tri[0].X, tc.tri[0].Y, 0}
+			tri1 := Point3d{tc.tri[1].X, tc.tri[1].Y, 0}
+			tri2 := Point3d{tc.tri[2].X, tc.tri[2].Y, 0}
+			intersect := PointTriangle3d(p, tri0, tri1, tri2)
+
+			i01 := PointLine3d(p, tri0, tri1)
+			i12 := PointLine3d(p, tri1, tri2)
+			i20 := PointLine3d(p, tri2, tri0)
+
+			intersect = intersect || i01 || i12 || i20
+
+			tint := 0 < len(tc.res)
+			if intersect != tint {
+				t.Errorf("not same: %v", tc)
+			}
+		})
+	}
+	t.Run("PT false", func(t *testing.T) {
+		intersect := PointTriangle3d(
+			Point3d{0, 0, 1},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if intersect {
+			t.Errorf("false")
+		}
+	})
+	t.Run("PT true", func(t *testing.T) {
+		intersect := PointTriangle3d(
+			Point3d{0, 0, 0},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if !intersect {
+			t.Errorf("false")
+		}
+	})
+	t.Run("ZeroT", func(t *testing.T) {
+		zero := ZeroTriangle3d(
+			Point3d{-1, 0, 0}, Point3d{-1, 0, 0}, Point3d{0, 10, 0},
+		)
+		if !zero {
+			t.Errorf("false")
+		}
+	})
+	t.Run("LT1 beg true", func(t *testing.T) {
+		intersect, _ := LineTriangle3dI1(
+			Point3d{0, 0, 0}, Point3d{0, 0, 1},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if !intersect {
+			t.Errorf("false")
+		}
+	})
+	t.Run("LT1 mid true", func(t *testing.T) {
+		intersect, _ := LineTriangle3dI1(
+			Point3d{0, 0, -1}, Point3d{0, 0, 1},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if !intersect {
+			t.Errorf("false")
+		}
+	})
+	t.Run("LT1 end true", func(t *testing.T) {
+		intersect, _ := LineTriangle3dI1(
+			Point3d{0, 0, -1}, Point3d{0, 0, 0},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if !intersect {
+			t.Errorf("false")
+		}
+	})
+
+	t.Run("LT1 not in tri false", func(t *testing.T) {
+		intersect, _ := LineTriangle3dI1(
+			Point3d{3, 3, -1}, Point3d{3, 3, 1},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if intersect {
+			t.Errorf("false")
+		}
+	})
+	t.Run("LT1 beg false", func(t *testing.T) {
+		intersect, _ := LineTriangle3dI1(
+			Point3d{0, 0, 0.1}, Point3d{0, 0, 1},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if intersect {
+			t.Errorf("false")
+		}
+	})
+	t.Run("LT1 end false", func(t *testing.T) {
+		intersect, _ := LineTriangle3dI1(
+			Point3d{0, 0, -1}, Point3d{0, 0, -0.1},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if intersect {
+			t.Errorf("false")
+		}
+	})
+	t.Run("LT1 on plane false", func(t *testing.T) {
+		intersect, _ := LineTriangle3dI1(
+			Point3d{0.1, 0.1, 0}, Point3d{-0.1, -0.1, 0},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if intersect {
+			t.Errorf("false")
+		}
+	})
+	t.Run("LT1 on parallel false", func(t *testing.T) {
+		intersect, _ := LineTriangle3dI1(
+			Point3d{0.1, 0.1, 1}, Point3d{-0.1, -0.1, 1},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if intersect {
+			t.Errorf("false")
+		}
+	})
+
+	t.Run("LT2 inside tri true", func(t *testing.T) {
+		intersect, ip := LineTriangle3dI2(
+			Point3d{-0.1, -0.1, 0}, Point3d{0.1, 0.1, 0},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if !intersect || len(ip) != 2 {
+			t.Errorf("false: %v", ip)
+		}
+	})
+	t.Run("LT2 inside_outside0 true", func(t *testing.T) {
+		intersect, ip := LineTriangle3dI2(
+			Point3d{-3, -2, 0}, Point3d{0.1, 0.1, 0},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if !intersect || len(ip) != 2 {
+			t.Errorf("false: %v", ip)
+		}
+	})
+	t.Run("LT2 inside_outside1 true", func(t *testing.T) {
+		intersect, ip := LineTriangle3dI2(
+			Point3d{-0.1, -0.1, 0}, Point3d{3, 2, 0},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if !intersect || len(ip) != 2 {
+			t.Errorf("false: %v", ip)
+		}
+	})
+
+	t.Run("LT2 outside on point true", func(t *testing.T) {
+		intersect, ip := LineTriangle3dI2(
+			Point3d{-3, 10, 0}, Point3d{0.1, 10, 0},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if !intersect || len(ip) != 2 {
+			t.Errorf("false: %v", ip)
+		}
+	})
+	t.Run("LT2 outside 1 false", func(t *testing.T) {
+		intersect, ip := LineTriangle3dI2(
+			Point3d{-3, 11, 0}, Point3d{0.1, 11, 0},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if intersect || len(ip) != 0 {
+			t.Errorf("false: %v", ip)
+		}
+	})
+	t.Run("LT2 outside 2 false", func(t *testing.T) {
+		intersect, ip := LineTriangle3dI2(
+			Point3d{-3, 3, 0}, Point3d{3, 3, 1},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 10, 0},
+		)
+		if intersect || len(ip) != 0 {
+			t.Errorf("false: %v", ip)
+		}
+	})
+
+	t.Run("TT 0 true", func(t *testing.T) {
+		intersect, ip := TriangleTriangle3d(
+			Point3d{-1.1, 0, -1}, Point3d{1.1, 0, -1}, Point3d{0, 0, 1},
+			Point3d{-1, -1, 0}, Point3d{1, -1, 0}, Point3d{0, 1, 0},
+		)
+		if !intersect || len(ip) != 2 {
+			t.Errorf("false: %v", ip)
+		}
+	})
+}
